1778feaa234b24b1a2162b2f97fca0422dfa7491
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-06-02 20:39:34 +0530

    added basic layout for UnitConverter

---
+++ unit_converter/lib/UnitConverter.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class UnitConverter extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        body: new Container(
+          color: Colors.brown,
+        ),
+      ),
+    );
+  }
+
+}
---
+++ unit_converter/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'UnitConverter.dart';
+
+void main(){
+  runApp(new UnitConverter());
+}
