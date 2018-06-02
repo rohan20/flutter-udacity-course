536c3d93e3c3f9716bbf09af753522f492382910
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-06-02 20:41:30 +0530

    added AppBar and removed banner

--- unit_converter/lib/UnitConverter.dart
+++ unit_converter/lib/UnitConverter.dart
@@ -2,16 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class UnitConverter extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: new Scaffold(
+        appBar: AppBar(
+          title: new Text("Unit Converter"),
+        ),
         body: new Container(
           color: Colors.brown,
         ),
       ),
     );
   }
-
 }
