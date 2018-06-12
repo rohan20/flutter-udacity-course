65086615317e138801b7a58906192a3322a453bf
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-06-12 09:43:25 +0530

    implemented category list item layout

--- unit_converter/lib/custom_widget/CategoryItem.dart
+++ unit_converter/lib/custom_widget/CategoryItem.dart
@@ -2,27 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 class CategoryItem extends StatelessWidget {
-
   String _categoryName;
 
-  CategoryItem({@required categoryName: ""}){
-   this._categoryName = categoryName;
+  CategoryItem({@required categoryName}) {
+    _categoryName = categoryName;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: new Container(
+    return Material(
+      color: Colors.green[100],
+      child: Container(
         height: 100.0,
-        child: new Row(
-          children: <Widget>[
-            new Icon(Icons.cake),
-            new Text(_categoryName),
-          ],
+        child: InkWell(
+          splashColor: Colors.lightBlue,
+          highlightColor: Colors.lightGreenAccent,
+          borderRadius: BorderRadius.circular(50.0),
+          onTap: () {
+            print("Tapped " + _categoryName);
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Container(
+              child: new Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: new Icon(
+                      Icons.cake,
+                      size: 60.0,
+                    ),
+                  ),
+                  new Text(
+                    _categoryName,
+                    style: TextStyle(fontSize: 24.0),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
-
 }
