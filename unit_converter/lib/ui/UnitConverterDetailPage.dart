5c03abfe9184be0390ab87f512a2aab31ce58077
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-06-14 04:56:43 +0530

    fix: returned empty container from detail page

--- unit_converter/lib/ui/UnitConverterDetailPage.dart
+++ unit_converter/lib/ui/UnitConverterDetailPage.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class UnitConverterDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
+    return new Container();
   }
 }
