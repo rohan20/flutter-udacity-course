5cc4c383fb599cafacfa23bfdc5e7c9ec2204ba3
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-06-16 14:10:16 +0530

    added inputDropdown items

--- unit_converter/lib/ui/ConverterPage.dart
+++ unit_converter/lib/ui/ConverterPage.dart
@@ -10,6 +10,22 @@ class ConverterPage extends StatelessWidget {
     _unitColor = unitColor;
   }
 
+  List<DropdownMenuItem<String>> _inputItems() {
+    List<DropdownMenuItem<String>> list = new List();
+    list.add(new DropdownMenuItem(child: Text("1")));
+    list.add(new DropdownMenuItem(child: Text("2")));
+    list.add(new DropdownMenuItem(child: Text("3")));
+    list.add(new DropdownMenuItem(child: Text("4")));
+    list.add(new DropdownMenuItem(child: Text("5")));
+    list.add(new DropdownMenuItem(child: Text("6")));
+    list.add(new DropdownMenuItem(child: Text("7")));
+    list.add(new DropdownMenuItem(child: Text("8")));
+    list.add(new DropdownMenuItem(child: Text("9")));
+    list.add(new DropdownMenuItem(child: Text("10")));
+
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -31,9 +47,29 @@ class ConverterPage extends StatelessWidget {
             color: Colors.blue[100],
             child: new Padding(
               padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: ConverterValue(
-                hintText: "Enter input value",
-                labelText: "Input",
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  new ConverterValue(
+                    hintText: "Enter input value",
+                    labelText: "Input",
+                  ),
+                  new SizedBox(height: 12.0),
+                  new Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                      ),
+                    ),
+                    child: DropdownButtonHideUnderline(
+                      child: ButtonTheme(
+                        child: new DropdownButton(
+                            items: _inputItems(), onChanged: null),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
